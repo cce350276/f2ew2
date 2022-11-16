2e460e8e7a53035cf89d3584efce3bf1b3bd8c8f
7N <cce350276@gmail.com>
2022-11-16 17:38:54 +0800

    step bar

---
+++ js/stepbar.js
@@ -0,0 +1,37 @@
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
+
+activeStep = 1;
+maxStep = 3;
+minStep = 1;
+
+checkButtonState();
+
+next.addEventListener('click', onNextClick);
+previous.addEventListener('click', onPreviousClick);
+
+function onNextClick() {  
+    activeStep = activeStep + 1;
+    document.querySelector(`[data-step="${activeStep}"]`).classList.add('active');
+    checkButtonState();
+}
+
+function onPreviousClick() {  
+    document.querySelector(`[data-step="${activeStep}"]`).classList.remove('active');
+    activeStep = activeStep - 1;
+    checkButtonState();
+}
+
+function checkButtonState() {
+  next.disabled= false;
+  previous.disabled= false;
+  
+  if(activeStep === maxStep) {
+       next.disabled = true;  
+  }
+  
+  if(activeStep === minStep) {
+      previous.disabled = true;
+  }
+
+}
