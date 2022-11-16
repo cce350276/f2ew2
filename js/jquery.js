b158ee3903348a9900ad3c3079d7bf3a537a8b1a
7N <cce350276@gmail.com>
2022-11-16 15:30:20 +0800

    模板

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,17 @@
+//popup
+
+$(".close").click(function () {
+  // 消失
+  $("#overly").removeClass("active");
+  $(".dialog").removeClass("active");
+  $(".overly-window").removeClass("active");
+});
+
+$(".open").click(function () {
+  $("#overly").toggleClass("active");
+  $(".popup").toggleClass("active");
+});
+
 
 
 
